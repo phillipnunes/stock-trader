57191c9627a3fa1ba51283861b3a512757138450
Phillip Nunes <phillip_nunes@hotmail.com>
2018-02-05 00:08:20 -0200

    Exibicao de dados estaticos

--- src/components/stocks/Stocks.vue
+++ src/components/stocks/Stocks.vue
@@ -1,3 +1,25 @@
 <template>
-  <h1>Stocks/Stocks component</h1>
+  <div>
+    <app-stock v-for="stock in stocks" :stock="stock" :key="stock.id"></app-stock>
+  </div>
 </template>
+
+<script>
+import Stock from './Stock'
+
+export default {
+  data () {
+    return {
+      stocks: [
+        {id: 1, name: 'AAA', price: 123},
+        {id: 2, name: 'BBB', price: 1231},
+        {id: 3, name: 'CCC', price: 2345},
+        {id: 4, name: 'DDD', price: 654}
+      ]
+    }
+  },
+  components: {
+    appStock: Stock
+  }
+}
+</script>
